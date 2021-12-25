eaa4e504706ea2521b7038e536bfe82b38b1fcd3
Vova Kolinko <vovakolinko@gmail.com>
2021-12-25 13:53:19 +0300

    try to enable autostart for ddos_conf

--- os/airootfs/etc/ddos_conf/autostart.sh
+++ os/airootfs/etc/ddos_conf/autostart.sh
@@ -1,8 +1,4 @@
-chmod +x /etc/ddos_conf/window_keys.sh
 sh /etc/ddos_conf/window_keys.sh
-chmod +x /etc/ddos_conf/gtk-theme.sh
 sh /etc/ddos_conf/gtk-theme.sh
-chmod +x /etc/ddos_conf/icon_pack.sh
 sh /etc/ddos_conf/icon_pack.sh
-chmod +x /etc/ddos_conf/wallpaper.sh
 sh /etc/ddos_conf/wallpaper.sh
