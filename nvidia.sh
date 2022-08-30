d45316ae01789e99e66dfd244ca5c049eb05603d
Vova <ddos@dev>
2022-08-30 21:05:44 +0300

    Nvidia

--- nvidia.sh
+++ nvidia.sh
@@ -1,5 +1,4 @@
 echo 'nvidia' >> os/packages.x86_64
 echo 'nvidia-utils' >> os/packages.x86_64
-mkdir ~/tmp
-sudo mkarchiso  -v -w ~/tmp -o ./release ./os
-sudo rm -R ~/tmp
+chmod +x build.sh
+./build.sh
